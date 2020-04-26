ce2aeac970c72e3a9df6d13939cc465b530e529d
beautifulpython <2426009680@qq.com>
2020-04-26 15:27:22 +0800

    mkdir

--- perf_xiaocai.py
+++ perf_xiaocai.py
@@ -6,11 +6,13 @@ from matplotlib.pyplot import MultipleLocator
 
 def fetch_log_files(dir):
   log_files=[]
+  func_name_list = []
   files = os.listdir(dir)
   for f in files:
     if os.path.splitext(f)[1] == '.txt':
       log_files.append(f)
-  return log_files
+      func_name_list.append(str(f).split(".")[0])
+  return func_name_list, log_files
 
 def fetch_func_perf(func_list,log_files):
   # declare var res_arrs based on func_name
@@ -43,18 +45,18 @@ def plot_perf_analysis(func_name, res_func_arr):
     perf_fig_title = str(func_name) + ' Performance Fig'
 
     # plot
-    index1 = list(range(1, len(res_func_arr) ))
+    index1 = list(range(1, len(res_func_arr)))
     plt.figure(figsize=(16, 4))  # picture size
     plt.plot(index1, res_func_arr[1:], marker='o', label="$10k$",linestyle='-', color="blue", linewidth=1)
-    for a,b in zip(index1,res_func_arr[1:]):
+    for a,b in zip(index1,res_func_arr):
         plt.text(a,b,round(b,3),ha='center',va='bottom',fontsize=12)
     plt.xlabel("Version ")  # X label
     plt.ylabel("Cost /ms")  # Y label
     plt.title(perf_fig_title)
     x0 = len(res_func_arr)
     y0 = res_func_arr[x0-1]
-    std_deviation = numpy.std(res_func_arr, ddof=1)
-    mean = numpy.mean(res_func_arr)
+    std_deviation = numpy.std(res_func_arr[1:], ddof=1)
+    mean = numpy.mean(res_func_arr[1:])
     # plt.annotate('std :%s\n'%std_deviation+'mean :%s'%mean, xy=(x0, y0), xytext=(+30, -30), textcoords='offset points', fontsize=12,
     #              arrowprops=dict(arrowstyle='->', connectionstyle='arc3,rad=.2'))
     plt.text(x0,y0,'std :%s\n'%round(std_deviation,5)+'mean :%s'%round(mean,5),bbox=dict(facecolor='g', edgecolor='gray', alpha=0.1),fontsize=14)
@@ -73,34 +75,56 @@ def plot_perf_analysis(func_name, res_func_arr):
 
 
 func_list_arr = [
+    'ST_Area',
+    'ST_AsText',
+    'ST_Buffer',
+    'ST_Centriod',
+    'ST_Contains',
+    'ST_Crosses',
+    'ST_Distance',
+    'ST_Envelope',
+    'ST_Equals',
+    'ST_GeometryType',
+    'ST_GeomFromWKT',
+    'ST_Intersects',
+    'ST_IsSimple',
+    'ST_IsValid',
+    'ST_Length',
+    'ST_LineStringFromText',
+    'ST_NPoints',
+    'ST_Overlaps',
+    'ST_PointFromText',
     'ST_Point',
-    'ST_IsValid'
+    'ST_PolygonFromText',
+    'ST_Touches',
+    'ST_WithIn'
     ]
-log_dir = 'perf/txt/'
-plot_dir = 'perf/pic/'
+log_dir = 'geomesa_report2/10_8/'
+plot_dir = 'geomesa_report2/pix_10_8/'
 # Performance analysis standard deviation accuracy tolerance
 std_threshold = 3.0
 
 # main invocation
 if __name__ == "__main__":
     # res_set is a list that contains historical performance data for all gis functions
-    res_set = fetch_func_perf(func_list_arr,fetch_log_files(log_dir))
-    assert len(func_list_arr) == len(res_set)
+    func_name_list, log_files = fetch_log_files(log_dir)
+    res_set = fetch_func_perf(func_name_list,log_files)
 
-    # produce specific result variable in main for every functions in func_list_arr
-    for i in range(0,len(func_list_arr)):
-      func_name = func_list_arr[i]
+    for i in range(0,len(func_name_list)):
+      func_name = func_name_list[i]
       exec('res_{}={}'.format(func_name,res_set[i]))
 
     alarm_num = 0
     plot_num = 0
     plot_failed_func = []
     alarm_func = []
-    for func_name in func_list_arr:
+    for func_name in func_name_list:
       res_arr = 'res_' + func_name
       # plot
       cur_res_arr = locals()[res_arr]
       if cur_res_arr:
+          print(cur_res_arr)
+          print(func_name)
           plot_perf_analysis(func_name,cur_res_arr)
           plot_num = plot_num + 1
           # performance test stability
